ccac8949d006234d9710ad46db32991cad5065a4
billy urrutia <billyurrutia@gmail.com>
2019-09-05 11:53:45 -0600

    added more functionality for the user

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
+  $('button').click(sendToAPI)
   // have fun!
   getTopWord(); 
 });
 
-var getTopWord = () => {
+function getTopWord() {
   fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
   .then(response => {
     return response.json()
@@ -18,11 +19,24 @@ var getTopWord = () => {
   })
 }
 
-var appendTopWord = (jsonReply) => {
+function appendTopWord(jsonReply) {
   $('.word-count').append(`
-    <h1>Top-Word</h1>
+    <h4>Top-Word</h4>
+    <br>
     <p>${Object.keys(jsonReply.word)}</p>
-    <h1>Top-count</h1>
+    <h4>Top-count</h4>
+    <br>
     <p>${Object.values(jsonReply.word)}</p>`
   ) 
 } 
+
+
+
+function sendToAPI(event){
+  event.preventDefault()
+  var words = $('textarea').val()
+  var data = { word: { value: words } }
+  $.post('https://wordwatch-api.herokuapp.com/api/v1/words', data ,function(){
+    alert('Added your essay to the Cloud')
+  })
+}
