eb8da624e5f051804796fab8b59207f6b778a94a
billy urrutia <billyurrutia@gmail.com>
2019-09-05 10:57:10 -0600

    Added top word to the right side of page

--- docs/bundle.js
+++ docs/bundle.js
@@ -83,8 +83,25 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 __WEBPACK_IMPORTED_MODULE_0_jquery___default()(document).ready(() => {
   // have fun!
-})
+  getTopWord(); 
+});
 
+getTopWord = () => {
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+  .then(response => {
+    return response.json()
+  })
+  .then(jsonReply => {
+    __WEBPACK_IMPORTED_MODULE_0_jquery___default()('.word-count').append(`
+    <h1>Top-Word</h1>
+    <p>${Object.keys(jsonReply.word)}</p>
+    <h1>Top-count</h1>
+    <p>${Object.values(jsonReply.word)}</p>`)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
 
 /***/ }),
 /* 2 */
--- src/index.js
+++ src/index.js
@@ -2,4 +2,27 @@ import $ from 'jquery'
 
 $(document).ready(() => {
   // have fun!
-})
+  getTopWord(); 
+});
+
+var getTopWord = () => {
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+  .then(response => {
+    return response.json()
+  })
+  .then(jsonReply => {
+    appendTopWord(jsonReply)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
+
+var appendTopWord = (jsonReply) => {
+  $('.word-count').append(`
+    <h1>Top-Word</h1>
+    <p>${Object.keys(jsonReply.word)}</p>
+    <h1>Top-count</h1>
+    <p>${Object.values(jsonReply.word)}</p>`
+  ) 
+} 
